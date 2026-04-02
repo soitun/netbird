9d1a37c64455798aa4b15b7404700d78993588c4
Bethuel Mmbaga <bethuelmbaga12@gmail.com>
2026-04-02 19:21:00 +0300

    [management,client] Revert gRPC client secret removal (#5781)
    * This reverts commit e5914e4e8b0ae030701529eb4f492d49eb10534b
    
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>
    
    * Deprecate client secret in proto
    
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>
    
    * Fix lint
    
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>
    
    ---------
    
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

--- client/internal/auth/auth.go
+++ client/internal/auth/auth.go
@@ -221,6 +221,7 @@ func (a *Auth) getPKCEFlow(client *mgm.GrpcClient) (*PKCEAuthorizationFlow, erro
 	config := &PKCEAuthProviderConfig{
 		Audience:              protoConfig.GetAudience(),
 		ClientID:              protoConfig.GetClientID(),
+		ClientSecret:          protoConfig.GetClientSecret(), //nolint:staticcheck
 		TokenEndpoint:         protoConfig.GetTokenEndpoint(),
 		AuthorizationEndpoint: protoConfig.GetAuthorizationEndpoint(),
 		Scope:                 protoConfig.GetScope(),
@@ -265,6 +266,7 @@ func (a *Auth) getDeviceFlow(client *mgm.GrpcClient) (*DeviceAuthorizationFlow, 
 	config := &DeviceAuthProviderConfig{
 		Audience:           protoConfig.GetAudience(),
 		ClientID:           protoConfig.GetClientID(),
+		ClientSecret:       protoConfig.GetClientSecret(), //nolint:staticcheck
 		Domain:             protoConfig.Domain,
 		TokenEndpoint:      protoConfig.GetTokenEndpoint(),
 		DeviceAuthEndpoint: protoConfig.GetDeviceAuthEndpoint(),
